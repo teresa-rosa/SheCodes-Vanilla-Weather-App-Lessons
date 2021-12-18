24208266a88c4a57a37c3d762a0b2bef94db54ed
teresa-rosa <teresarosa28@gmail.com>
2021-12-18 23:39:02 +0000

    updated-to-current-data

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "8c362dd932bee69aa7eece7fea98811a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
